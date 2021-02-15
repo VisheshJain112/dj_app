920b18cf127fe211de12696caf7a2a779e88e3c4
= <=>
2021-02-15 21:29:50 +0530

    New changes

--- smartedukart/settings.py
+++ smartedukart/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'mainhome.apps.MainhomeConfig',
-    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- smartedukart/urls.py
+++ smartedukart/urls.py
@@ -19,12 +19,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',include('mainhome.urls')),
-    path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls')),
-    path('world/',include('world.urls')),
-    path('activity/', include('activity.urls')),
-    path('openbook/', include('openbook.urls'))
-    
-    
-]
+    ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
